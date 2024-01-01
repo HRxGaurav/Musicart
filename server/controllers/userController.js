a43004d4761e3d4169c1e0a1ac25f479ee351115
HRxGaurav <magicgalaxy0@gmail.com>
2024-01-01 17:50:41 +0530

    feat-added validator for email phone

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -8,30 +8,41 @@ dotenv.config();
 
 
 const register = async (req, res) => {
-    const { name, phone, email,  password } = req.body;
+    const { name, phone, email, password } = req.body;
 
     if (!name || !password || !phone || !email) {
         return res.status(422).json({ error: "Please fill in all fields properly" });
     }
 
     try {
+        // Validate email format
+        if (!validator.isEmail(email)) {
+            return res.status(400).json({ error: 'Invalid email format' });
+        }
+
+        // Validate phone format (you may need to adjust this based on your specific requirements)
+        if (!validator.isMobilePhone(phone, 'any', { strictMode: false })) {
+            return res.status(400).json({ error: 'Invalid phone format' });
+        }
+
         const userExist = await User.findOne({ email });
 
         if (userExist) {
             return res.status(422).json({ error: "User already registered" });
         } else {
             const hashPassword = await bcrypt.hash(password, 10);
-            const newUser = new User({ name, phone, email, password: hashPassword});            
+            const newUser = new User({ name, phone, email, password: hashPassword });
             await newUser.save();
-            const userLogin = await User.findOne({ email })
-            const token = jwt.sign({ userID: userLogin._id }, process.env.JWT_SECRET_KEY, { expiresIn: '7d' })
-            res.status(201).json({ message: "User registered successfully", email:email, token:token, id:userLogin._id });
+            const userLogin = await User.findOne({ email });
+            const token = jwt.sign({ userID: userLogin._id }, process.env.JWT_SECRET_KEY, { expiresIn: '7d' });
+            res.status(201).json({ message: "User registered successfully", email: email, token: token, id: userLogin._id });
         }
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: "Server error" });
     }
 };
+
 
 const login = async (req, res) => {
     const { username, password } = req.body;
