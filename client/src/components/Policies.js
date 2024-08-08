16211112df51577039ef165614ba131479f8fe74
hrxgaurav <magicgalaxy0@gmail.com>
2024-08-08 11:56:39 +0530

    tnc

--- client/src/components/Policies.js
+++ client/src/components/Policies.js
@@ -4,6 +4,8 @@ const Policies = () => {
   const containerStyle = {
     padding: '20px',
     fontFamily: 'Arial, sans-serif',
+    maxWidth: '60%', // Set maximum width to 60%
+    margin: '0 auto', // Center the container
   };
 
   const sectionStyle = {
@@ -14,22 +16,23 @@ const Policies = () => {
     color: '#333',
     borderBottom: '2px solid #ddd',
     paddingBottom: '5px',
-    fontSize: '18px',
+    fontSize: '24px', // Increase font size
   };
 
   const listText = {
     color: '#555',
-    fontSize: '16px',
+    fontSize: '18px', // Increase font size
   };
 
   const policyText = {
     color: '#555',
-    fontSize: '16px',
+    fontSize: '18px', // Increase font size
+    lineHeight: '1.6', // Improve readability
   };
 
   const strongText = {
     color: '#555',
-    fontSize: '16px',
+    fontSize: '18px', // Increase font size
   };
 
   return (
@@ -69,22 +72,50 @@ const Policies = () => {
       <div style={sectionStyle}>
         <h2 style={headingStyle}>Privacy Policy</h2>
         <p style={policyText}>
-          This Privacy Policy explains how we collect, use, and disclose your personal information when you visit or make a purchase from our e-commerce website.
+          Our privacy policy is simple: any information you give us stays with us. 
         </p>
         <p style={policyText}>
-          <strong style={strongText}>Information We Collect:</strong> We collect personal information that you provide to us when you register on our website, place an order, subscribe to our newsletter, or contact us. This information may include your name, email address, shipping address, and payment information.
+          When you use our Website, we collect and store your personal information which is provided by you from time to time. Our primary goal in doing so is to provide you a safe, efficient, smooth, and customized experience. This allows us to provide services and features that most likely meet your needs, and to customize our Website to make your experience safer and easier. More importantly, while doing so, we collect personal information from you that we consider necessary for achieving this purpose.
         </p>
         <p style={policyText}>
-          <strong style={strongText}>How We Use Your Information:</strong> We use the information we collect to process your orders, communicate with you, improve our website, and for marketing purposes. We do not sell or share your personal information with third parties except as necessary to provide our services or comply with the law.
+          In general, you can browse the Website without telling us who you are or revealing any personal information about yourself. Once you give us your personal information, you are not anonymous to us.
+        </p>
+        <h3 style={headingStyle}>Limited Data Access</h3>
+        <p style={policyText}>
+          Within our organization, your personal data is accessible to only a limited number of employees with special access privileges. Although we may, from time to time, compile general demographic information based on your order, this information is shared within our organization only and has no identifiable personal data associated with it.
+        </p>
+        <h3 style={headingStyle}>Information Collected</h3>
+        <p style={policyText}>
+          To enable you to place an order on our site, we need to have the following basic information about you: First Name, Last Name, Address, City, Zip code, State, Country, Phone Number, and Contact E-mail address. Apart from this, our systems gather certain details about your computer's internet connection like your IP address when you visit our site. Your IP address does not identify you personally. We use this information to deliver our web pages to you upon request, to customize our site as per your interest, to calculate the number of visitors on our site, and to know the geographic locations from where our visitors come.
         </p>
         <p style={policyText}>
-          <strong style={strongText}>Data Security:</strong> We implement a variety of security measures to maintain the safety of your personal information. Your payment information is processed securely through our payment gateway providers.
+          We do not allow any unauthorized person or organization, be it other members, visitors, or anyone not in our organization, to use any information collected from you. Note: Our privacy policy is subject to change at any time without notice.
         </p>
         <p style={policyText}>
-          <strong style={strongText}>Cookies:</strong> We use cookies to enhance your experience on our website, analyze site traffic, and personalize content. You can choose to disable cookies through your browser settings.
+          By visiting this Website, you agree to be bound by the terms and conditions of this Privacy Policy. If you do not agree, please do not use or access our Website.
+        </p>
+        <h3 style={headingStyle}>Information Modification</h3>
+        <p style={policyText}>
+          You can modify your personal details by accessing the Edit Profile option at any time from our website.
+        </p>
+        <h3 style={headingStyle}>Information Sharing</h3>
+        <p style={policyText}>
+          Probably this is the most important question in your mind as to with whom do we share your information? We may share personal information with our other corporate entities and affiliates. These entities and affiliates may market to you as a result of such sharing. To some extent, information has to be passed on to the courier companies, credit card processing companies, vendors, etc. to enable them to perform their functions related to your order fulfillment. Apart from this normal business requirement, information may also be needed to be shared with law authorities, for fraud detection, and for the safety of our website, employees, management, users, members, and other affiliates associated with us.
+        </p>
+        <h3 style={headingStyle}>Links to Other Sites</h3>
+        <p style={policyText}>
+          Our Website links to other websites that may collect personally identifiable information about you. We are not responsible for the privacy practices or the content of those linked websites.
+        </p>
+        <h3 style={headingStyle}>Information Usage</h3>
+        <p style={policyText}>
+          The most important usage of the information collected from you is your email address, which is used to inform you that your order has been confirmed/executed. Your email address is also used to inform you about your customer service-related queries and for any newsletters sent. All other information collected is confidentially stored and will not be disclosed unless needed as per the requirement of the law authorities or in case of any disputes.
         </p>
         <p style={policyText}>
-          <strong style={strongText}>Changes to This Policy:</strong> We may update this Privacy Policy from time to time. Any changes will be posted on this page with an updated revision date.
+          We will occasionally ask you to complete optional online surveys. These surveys may ask you for contact information and demographic information. We use this data to tailor your experience at our Website, providing you with content that we think you might be interested in and to display content according to your preferences.
+        </p>
+        <h3 style={headingStyle}>Your Consent</h3>
+        <p style={policyText}>
+          By using the Website and/or by providing your information, you consent to the collection and use of the information you disclose on the Website in accordance with this Privacy Policy, including but not limited to your consent for sharing your information as per this privacy policy.
         </p>
       </div>
 
